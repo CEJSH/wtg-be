ad7aba3cb9949cb53f257cc38b90075cd6c6d8c5
CEJSH <eun2763@naver.com>
2024-08-02 13:25:04 +0900

    feature: Delete useless things

--- src/enum/roles.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum RolesEnum {
-  Master = '마스터',
-  Manager = '관리자',
-  User = '사용자',
-  None = '비회원',
-}
--- src/guard/roles/roles.guard.ts
+++ src/guard/roles/roles.guard.ts
@@ -1,18 +1,11 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Request } from 'express';
-import { Exception } from 'src/class/exception/exception';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    // const roles = this.reflector.get(Roles, context.getHandler());
-    // if (!roles) {
-    //   return true;
-    // }
-
     const roles = this.reflector.getAllAndOverride<string[]>('roles', [
       context.getClass(),
       context.getHandler(),
@@ -21,22 +14,5 @@ export class RolesGuard implements CanActivate {
     if (roles.length === 0) {
       return true;
     }
-
-    // const request = context.switchToHttp().getRequest<Request>();
-    // if (request?.user) {
-    //   const user = request.user as Partial<UserEntity>;
-
-    //   if (roles.includes(user.role)) {
-    //     return true;
-    //   } else {
-    //     throw Exception(
-    //       'AccountError5',
-    //       AccountError5,
-    //       new Error(`${user.role}의 api 접근 권한을 확인해주세요.`),
-    //     );
-    //   }
-    // } else {
-    //   throw Exception('AccountError4', AccountError4);
-    // }
   }
 }
